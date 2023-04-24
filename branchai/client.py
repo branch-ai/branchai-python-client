d3c1ed25a35744357c86ee8138422153b33bba96
praveen07 <praveen.ork@gmail.com>
2023-04-24 15:28:48 +0800

    initial version

---
+++ branchai/client.py
@@ -0,0 +1,46 @@
+import requests
+
+
+class Client:
+    def __init__(self, url: str):
+        self.url = url
+        self.pipeline_id = None
+        self.destination_id = None
+
+    def with_pipeline(self, pipeline_id: int):
+        self.pipeline_id = pipeline_id
+        return self
+
+    def with_destination(self, destination_id: int):
+        self.destination_id = destination_id
+        return self
+
+    def search(self, query: str, top_k: int = 1):
+        if not self.pipeline_id and not self.destination_id:
+            raise TypeError("Missing pipeline or destination, try with_pipeline or with_destination")
+        payload = {
+            "query": query,
+            "top_k": top_k,
+            "pipeline_id": self.pipeline_id,
+            "destination_id": self.destination_id
+        }
+        response = requests.post(f"{self.url}/search", json=payload)
+        return response
+
+    def generate(self, query: str, top_k: int = 1):
+        payload = {
+            "query": query,
+            "top_k": top_k,
+            "pipeline_id": self.pipeline_id,
+            "destination_id": self.destination_id
+        }
+        response = requests.post(f"{self.url}/generate", json=payload)
+        return response
+
+
+"""
+import branchai
+client = branchai.client("server_url")
+destination = client.with_pipeline(pipeline_id) || client.with_destination(destination_id)
+results = destination.search("what ....")
+"""
