6440a508b68aed7c6c3fd76f3cf98c99c643dc53
praveen07 <praveen.ork@gmail.com>
2023-04-25 10:09:20 +0800

    Add adapter class to wrap around pipeline and destination

--- branchai/client.py
+++ branchai/client.py
@@ -1,19 +1,11 @@
 import requests
 
 
-class Client:
-    def __init__(self, url: str):
+class Adapter:
+    def __init__(self, url, pipeline_id=None, destination_id=None):
         self.url = url
-        self.pipeline_id = None
-        self.destination_id = None
-
-    def with_pipeline(self, pipeline_id: int):
         self.pipeline_id = pipeline_id
-        return self
-
-    def with_destination(self, destination_id: int):
         self.destination_id = destination_id
-        return self
 
     def search(self, query: str, top_k: int = 1):
         if not self.pipeline_id and not self.destination_id:
@@ -39,4 +31,11 @@ class Client:
         response = requests.post(f"{self.url}/generate", json=payload)
         return response
 
+
+class Client:
+    def __init__(self, url: str):
+        self.url = url
+
+    def with_adapter(self, pipeline_id: int, destination_id: int = None) -> Adapter:
+        return Adapter(self.url, pipeline_id=pipeline_id, destination_id=destination_id)
 
