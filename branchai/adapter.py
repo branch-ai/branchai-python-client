54721b9ca85d3a3ed0bfe8625790107e93998221
praveen07 <praveen.ork@gmail.com>
2023-05-01 16:37:12 +0800

    Add system_prompt and temperature as parameters for generate

--- branchai/adapter.py
+++ branchai/adapter.py
@@ -19,12 +19,14 @@ class Adapter:
         response = requests.post(f"{self.server_url}/search", json=payload)
         return response
 
-    def generate(self, query: str, top_k: int = 1):
+    def generate(self, query: str, top_k: int = 1, system_prompt: str = None, temperature: float = 0):
         if not self.pipeline_id and not self.destination_id:
             raise TypeError("Missing pipeline or destination, try with_pipeline or with_destination")
         payload = {
             "query": query,
+            "system_prompt": system_prompt,
             "top_k": top_k,
+            "temperature": temperature,
             "pipeline_id": self.pipeline_id,
             "destination_id": self.destination_id
         }
