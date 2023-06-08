5b48445d38a5eb994c3ffe7260d24701c8f64ff5
praveen07 <praveen.ork@gmail.com>
2023-06-08 19:14:24 +0800

    add api token in request

--- branchai/adapter.py
+++ branchai/adapter.py
@@ -4,10 +4,11 @@ import requests
 
 
 class Adapter:
-    def __init__(self, server_url, pipeline_id=None, destination_id=None):
+    def __init__(self, server_url, pipeline_id=None, destination_id=None, headers={}):
         self.server_url = server_url
         self.pipeline_id = pipeline_id
         self.destination_id = destination_id
+        self.headers = headers
 
     def search(self, query: str, top_k: int = 1, certainty: float = 0.5, user_id: str = ""):
         if not self.pipeline_id and not self.destination_id:
@@ -20,10 +21,10 @@ class Adapter:
             "pipeline_id": self.pipeline_id,
             "destination_id": self.destination_id
         }
-        response = requests.post(f"{self.server_url}/search", json=payload)
+        response = requests.post(f"{self.server_url}/v1/search", json=payload, headers=self.headers)
         return response
 
-    def generate(self, query: str, top_k: int = 1, system_prompt: str = None, temperature: float = 0,
+    def generate(self, query: str, top_k: int = 2, system_prompt: str = None, temperature: float = 0,
                  session_id: str = None, user_id: str = "", query_id: Optional[str] = None):
         if not self.pipeline_id and not self.destination_id:
             raise TypeError("Missing pipeline or destination, try with_pipeline or with_destination")
@@ -38,7 +39,7 @@ class Adapter:
             "destination_id": self.destination_id,
             "session_id": session_id
         }
-        response = requests.post(f"{self.server_url}/generate", json=payload)
+        response = requests.post(f"{self.server_url}/v1/generate", json=payload, headers=self.headers)
         return response
 
     def reason(self, query: str, temperature: float = 0, session_id: str = None, user_id: str = ""):
@@ -52,5 +53,5 @@ class Adapter:
             "destination_id": self.destination_id,
             "session_id": session_id
         }
-        response = requests.post(f"{self.server_url}/reason", json=payload)
+        response = requests.post(f"{self.server_url}/v1/reason", json=payload, headers=self.headers)
         return response
--- branchai/client.py
+++ branchai/client.py
@@ -1,13 +1,22 @@
+from typing import Optional
+
+import requests
+
 from branchai.adapter import Adapter
 from branchai.recipe import Recipe
 
 
+
+
 class Client:
-    def __init__(self, server_url: str):
+    def __init__(self, server_url: str, api_token: Optional[str] = None):
         self.server_url = server_url
+        self.headers = {}
+        if api_token:
+            self.headers['Authorization'] = f"Bearer {api_token}"
 
     def with_adapter(self, pipeline_id: int, destination_id: int = None) -> Adapter:
-        return Adapter(self.server_url, pipeline_id=pipeline_id, destination_id=destination_id)
+        return Adapter(self.server_url, pipeline_id=pipeline_id, destination_id=destination_id, headers=self.headers)
 
     def with_recipe(self, template: str) -> Recipe:
         return Recipe(self.server_url, template)
